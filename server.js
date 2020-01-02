0ef86bf88ffa00f261383f638ba82c3bdd17afd3
= <zainr7989@gmail.com>
2020-01-02 15:31:43 -0500

    [Add] Add GET route for create post route.

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.listen(PORT, () => console.log(`I'm listening on port ${PORT}`));
 // Route for Home page
 app.get('/', (req, res) => res.render('index'));
 
+// 'Create post' route views (GET and POST)
+app.get('/posts/new', (req, res) => res.render('posts-new'));
+
 // Handlebars middleware
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
