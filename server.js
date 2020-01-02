1d0ffdafe272babee88c6d8ad8618b833c2c9382
= <zainr7989@gmail.com>
2020-01-02 14:48:40 -0500

    [Update] Reddit.js index using Handlebars.

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 // Get a basic Hello world site up
 const express = require('express');
 const app = express();
+// import Handlebars
+const exphbs = require('express-handlebars');
 
+// give app a port
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(PORT, () => console.log(`I'm listening on port ${PORT}`));
+
+// Route for Home page
+app.get('/', (req, res) => res.render('index'));
+
+// Handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
